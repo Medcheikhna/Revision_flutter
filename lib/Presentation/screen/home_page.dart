dd0fe4ec3555f3e588967d6700ef58decee3eb7f
Medcheikhna <mohamedsidili77@gmail.com>
2025-03-14 14:52:36 +0000

    designe changes

--- lib/Presentation/screen/home_page.dart
+++ lib/Presentation/screen/home_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
-import 'package:newtest/presentation/widget/custom_drawer.dart';
-
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../view_model/user_view_model.dart';
+import '../widget/custom_drawer.dart';
 import '../widget/widget_home_page.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -15,42 +13,74 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final userViewModel = context.watch<UserViewModel>();
-   
-    
+
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.userManagement)),
-      drawer: CustomDrawer(),
-      body: userViewModel.users.isEmpty && userViewModel.isLoading
-          ? const Center(child: CircularProgressIndicator(color: Colors.amber))
-          : ListView.builder(
-              controller: userViewModel.scrollController,
-              itemCount: userViewModel.users.length +
-                  (userViewModel.isLoading ? 1 : 0),
-              itemBuilder: (context, index) {
-                if (index < userViewModel.users.length) {
-                  final user = userViewModel.users[index];
-                  return ListTile(
-                    title: Text(
-                        "${AppLocalizations.of(context)!.name}${user.username}"),
-                    subtitle: Text(
-                        "${AppLocalizations.of(context)!.email} ${user.email}"),
-                    onTap: () => UserDialog.show(context, user),
-                  );
-                } else {
-                  return const Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: CircularProgressIndicator(color: Colors.amber),
-                    ),
-                  );
-                }
-              },
-            ),
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.userManagement,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        elevation: 4,
+      ),
+      drawer: const CustomDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: userViewModel.users.isEmpty && userViewModel.isLoading
+            ? const Center(
+                child: CircularProgressIndicator(color: Colors.orange),
+              )
+            : ListView.builder(
+                controller: userViewModel.scrollController,
+                itemCount: userViewModel.users.length +
+                    (userViewModel.isLoading ? 1 : 0),
+                itemBuilder: (context, index) {
+                  if (index < userViewModel.users.length) {
+                    final user = userViewModel.users[index];
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(16),
+                        leading: CircleAvatar(
+                          child: Text(user.username![0].toUpperCase(),
+                              style: const TextStyle(color: Colors.white)),
+                        ),
+                        title: Text(
+                          "${AppLocalizations.of(context)!.name} ${user.username}",
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        subtitle: Text(
+                          "${AppLocalizations.of(context)!.email} ${user.email}",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        trailing: const Icon(
+                          Icons.arrow_forward_ios,
+                          size: 18,
+                        ),
+                        onTap: () => UserDialog.show(context, user),
+                      ),
+                    );
+                  } else {
+                    return const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: CircularProgressIndicator(color: Colors.orange),
+                      ),
+                    );
+                  }
+                },
+              ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.push('/add_user');
-        },
-        child: const Icon(Icons.add),
+        onPressed: () => context.push('/add_user'),
+        child: const Icon(
+          Icons.add,
+        ),
       ),
     );
   }
